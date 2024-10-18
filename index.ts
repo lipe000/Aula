ba87ae2b01f678915d1eee836a2c3b835630bf4b
lipe000 <lipebianchi10@gmail.com>
2024-10-17 21:50:43 -0300

    meu primeiro commit

---
+++ index.ts
@@ -0,0 +1,7 @@
+console.log("Olá");
+
+//git init -> Inicializa o git
+/*git remote add origin -> Cria um link entre o repositorio local
+e o repositorio no provedor de repositorios */
+//git add -> Adiciona os arquivos no repositorio
+//git status -> Lista os arquivos adicionados/modificados
